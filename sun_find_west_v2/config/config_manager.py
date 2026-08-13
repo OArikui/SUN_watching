aadcd84dad68c727c2374f3a9a00e38c92c2b46c
OArikui <Arikui57@outlook.jp>
2026-08-14 01:33:07 +0900

    feat:config

---
+++ sun_find_west_v2/config/config_manager.py
@@ -0,0 +1,13 @@
+#part1 importing modules
+
+#func1 setting the parameter with GUI
+
+#part2 saving parameter json from txtfile and load the json.(if not __name__=="__main__")
+
+#   for bury the empty,prepare defult json
+
+#part3 if saving or loading failed,suggesting func1 to user(or error and finish process)
+
+#part3 else styling parameters as a argument
+
+#(find_west:from find_west_setting import parameters)
