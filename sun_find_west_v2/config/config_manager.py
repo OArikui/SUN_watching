94087bd99f714da85f1d5ff09dd53549a9de1cca
OArikui <Arikui57@outlook.jp>
2026-08-14 19:22:36 +0900

    feat:textからjsonを生成

--- sun_find_west_v2/config/config_manager.py
+++ sun_find_west_v2/config/config_manager.py
@@ -11,12 +11,72 @@ if GUISET:
     )
 
 logger = logging.getLogger(__name__)
+
+CONFIG_TEXT_PATH=Path("config/config/config.txt")
+CONFIG_JSON_PATH=Path("config/config/config.txt")
+DEFAULT_JSON_PATH=Path("config/defaults/default_config.json")
+OUTLINE_JSON_PARH=Path("config/defaults/default_outline.json")
+CONFIG_SCHEMA_PATH=Path("config/schemas/config_schema.json")
+
+from config.utils_json import json_saver,json_loader
+
+def gem_config_json(textpath: Path, jsonpath: Path)->dict:
+    if not textpath.exist():
+        logger.error(f"file not found:{str(textpath)}")
+
+	with open(textpath,"r",encoding="utf-8") as f:
+		text=f.read().split("\n")
+
+	stash={}
+	data_h1={}
+	data_h2={}
+	h1=""
+	h2=""
+	for i,line in enumerate(text):
+		if not line.headswith("---","==="):
+			cleaned=""
+			for c in line:
+				if c == " ":
+					continue
+				cleaned+=c
+			k,vd=tuple(cleaned.split(":"))
+			v,d=tuple(vd.split("/"))
+			stash[k]=v
+			stash[k+"-desc"]=d
+		if line.headswith("---") or i=len(text)-1:
+			if stash:
+				data_h2[h2]=stash
+			h2=line.split("---")[1]
+			stash={}
+		if line.headswith("===") or i=len(text)-1:
+			if data_h2:
+				data_h1[h1]=data_h2
+			h1=line.split("===")[1]
+			data_h2={}
+	return json_saver(data=data_h1,jsonpath=jsonpath)
+
 # func1 setting the parameter with GUI
 
 # part2 saving parameter json from txtfile and load the json.(if not __name__=="__main__")
+try:
+	data=gem_config_json(CONFIG_TEXT_PATH,CONFIG_JSON_PATH)
+	logger.info("__sucessful jenelate json from text")
+except (KeyError,ValueError):
+	logger.exception("__config text not TEKISETU")
+	logger.info("__try load parameter from config json")
 
+try:
+	logger.info("__loading config from json")
+	data=json_loader(CONFIG_JSON_PATH)
+except:
+	logger.error("__failed loading config from json")
 #   for bury the empty,prepare defult json
 
+try:
+	default_config=json_loader(DEFAULT_JSON_PATH)
+	logger.info("__sucessful default_config")
+except:
+	logger.warn("__failed loading default_config")
 # part3 if saving or loading failed,suggesting func1 to user(or error and finish process)
 
 # part3 else styling parameters as a argument
