a159427ce0c8a13fbfc07b6bb9feae34400392e4
OArikui <Arikui57@outlook.jp>
2026-08-19 22:59:49 +0900

    run/feat: config add MIN2 param #2 #6
    ransacのパラメータは、module側が受け付けていないので、これから。
    manage_configとcache_updateを実行

--- sun_find_west_v2/config/pathes.py
+++ sun_find_west_v2/config/pathes.py
@@ -3,7 +3,7 @@ from pathlib import Path
 pathes = {
     "CONFIG_TEXT_PATH": Path("user_set/config.txt"),
     "CONFIG_JSON_PATH": Path("cache/config.json"),
-    "DEFAULT_JSON_PATH": Path("cache/default_config.json"),
+    "DEFAULT_JSON_PATH": Path("schemas/default_config.json"),
     "OUTLINE_JSON_PARH": Path("schemas/default_outline.json"),
     "CONFIG_SCHEMA_PATH": Path("schemas/config_schema.json"),
 }
