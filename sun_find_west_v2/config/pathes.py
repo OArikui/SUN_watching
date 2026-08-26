a8409e142936f834adfeca4827cfb37b1ab34ada
Mangostin <268108073+fujitenmon-coder@users.noreply.github.com>
2026-08-27 04:43:02 +0900

    reset

--- sun_find_west_v2/config/pathes.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-pathes = {
-    "CONFIG_TEXT_PATH": Path("user_set/config.txt"),
-    "CONFIG_JSON_PATH": Path("cache/config.json"),
-    "DEFAULT_JSON_PATH": Path("schemas/default_config.json"),
-    "OUTLINE_JSON_PARH": Path("schemas/default_outline.json"),
-    "CONFIG_SCHEMA_PATH": Path("schemas/config_schema.json"),
-}
